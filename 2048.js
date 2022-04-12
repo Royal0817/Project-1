abb1ebe073a957ae347fd874b11c82cf148df822
royal0817 <martinezl27737@gmail.com>
2022-04-12 15:50:20 -0400

    added the check for win

--- 2048.js
+++ 2048.js
@@ -1,89 +1,86 @@
 document.addEventListener('DOMContentLoaded', () => {
- const container = document.querySelector('.grid-container')
- const width = 4
- const height = 4
- let cells = []
-
- //creates grid of cells 
- // createGrid = [(index0),(index1),(index2),(index3),
-            //    (index4),(index5),(index6),(index7),
-            //    (index8),(index9),(index10),(index11),
-            //    (index12),(index13),(index14),(index15)]
+    const container = document.querySelector('.grid-container')
+    const width = 4
+    const height = 4
+    let cells = []
+    //winCondition()
+    //creates grid of cells 
+    // createGrid = [(index0),(index1),(index2),(index3),
+    //               (index4),(index5),(index6),(index7),
+    //               (index8),(index9),(index10),(index11),
+    //               (index12),(index13),(index14),(index15)]
     function createGrid() {
-        for(let i = 0; i < width*height; i++){
-            cell = document.createElement('grid-cell')
-            cell.textContent = 0
-            container.append(cell)
-            cells.push(cell)     
-           
-        }
+           for(let i = 0; i < width*height; i++){
+               cell = document.createElement('grid-cell')
+               cell.textContent = 0
+               container.append(cell)
+               cells.push(cell)     
+              
+           }
     }
     createGrid()
- //generates random number in grid container. else statement is in case random cell is not equal to 0
+
+    //generates random number in grid container. {} else statement is in case random cell is not equal to 0
     function generateNumbers() {
         let randomCell = Math.floor(Math.random() * cells.length)
-        if (cells[randomCell].textContent == 0){
-        cells[randomCell].textContent = 20
+           if (cells[randomCell].textContent == 0){
+           cells[randomCell].textContent = 2
         } else generateNumbers()
     }
-
+   
     //arrow keys to move divs, game controls
     document.onkeydown = function (e) {
-        console.log(e)
-        switch (e.key) {
-            //left arrow key
-            case 'ArrowLeft':
-                moveLeft()
-                combineRowLeftRight()
-                moveLeft()
-                generateNumbers()
+           console.log(e)
+           switch (e.key) {
+               //left arrow key
+               case 'ArrowLeft':
+                   moveLeft()
+                   combineRowLeftRight()
+                   moveLeft()
+                   generateNumbers()
+                   break;
+   
+               //up arrow key
+               case 'ArrowUp':
+                   moveUp()
+                   combineRowUpDown()
+                   moveUp()
+                   generateNumbers()
+                   
+                   break;
+   
+                //right arrow key
+               case 'ArrowRight':
+                   moveRight()
+                   combineRowLeftRight()
+                   moveRight()
+                   generateNumbers()
                 break;
+   
+                // down arrow key 
+               case 'ArrowDown':
+                   moveDown()
+                   combineRowUpDown()
+                   moveDown()
+                   generateNumbers()
+                   
+                   break;
+            }
+   
+    }
 
-            //up arrow key
-            case 'ArrowUp':
-                moveUp()
-                combineRowUpDown()
-                moveUp()
-                generateNumbers()
-                break;
-
-             //right arrow key
-            case 'ArrowRight':
-                moveRight()
-                combineRowLeftRight()
-                moveRight()
-                generateNumbers()
-             break;
-
-             // down arrow key 
-            case 'ArrowDown':
-                moveDown()
-                combineRowUpDown()
-                moveDown()
-                generateNumbers()
-                
-                break;
-        }
-
-    
-        
         function moveRight() {
             for (let i=0; i < width * height; i++) {
-               //allows computer get rows on left hand side of the screen 
-              if (i % 4 === 0) {
+                //allows computer get rows on left hand side of the screen 
+                if (i % 4 === 0) {
                     //grabs column on left hand side and defines arrays in the row as a string
                     let totalOne = cells[i].textContent
-                    let totalTwo = cells[i + 1].textContent 
+                    let totalTwo = cells[i + 1].textContent
                     let totalThree = cells[i + 2].textContent
                     let totalFour = cells[i + 3].textContent
                     //makes the row into a complete array, parseInt coverts strings to a number(will help later to add values together)
                     let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-                    // console.log(totalOne)
-                    // console.log(totalTwo)
-                    // console.log(totalThree)
-                    // console.log(totalFour)
-                    // console.log(row)
-
+               
                     //weeds out numbers that are not 0
                     let filteredArray = row.filter(cell => cell)
                     // fills in cells without a number with a 0 by filtering through array to check which ones a re missing
@@ -105,8 +102,8 @@ document.addEventListener('DOMContentLoaded', () => {
     
         function moveLeft() {
             for (let i=0; i < width * height; i++) {
-               //allows computer get rows on left hand side of the screen 
-              if (i % 4 === 0) {
+                //allows computer get rows on left hand side of the screen 
+                if (i % 4 === 0) {
                     //grabs column on left hand side and defines cells in the row 
                     let totalOne = cells[i].textContent
                     let totalTwo = cells[i + 1].textContent 
@@ -137,29 +134,29 @@ document.addEventListener('DOMContentLoaded', () => {
         function moveUp() {
             for (let i=0; i < height; i++) {
                 
-                    //grabs column on left hand side and defines cells in the row 
-                    let totalOne = cells[i].textContent
-                    let totalTwo = cells[i + height].textContent 
-                    let totalThree = cells[i + (height * 2)].textContent
-                    let totalFour = cells[i + (height * 3)].textContent
-                    //makes the column into an array, parseInt coverts strings to a number
-                    let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-                    // console.log(totalOne)
-                    // console.log(totalTwo)
-                    // console.log(totalThree)
-                    // console.log(totalFour)
-                    //weeds out numbers 
-                    let filteredArray = row.filter(cell => cell)
-                    let missing = 4 - filteredArray.length
-                    let zeros = Array(missing).fill(0)
-                    //changed placement zeros and filtered row to move left
-                    let newRow = filteredArray.concat(zeros)
-                        
-                    //assigns new value of array in indexes  
-                    cells[i].textContent = newRow[0]
-                    cells[i + height].textContent = newRow[1]
-                    cells[i + (height * 2)].textContent = newRow[2]
-                    cells[i + (height * 3)].textContent = newRow[3]
+                //grabs column on left hand side and defines cells in the row 
+                let totalOne = cells[i].textContent
+                let totalTwo = cells[i + height].textContent 
+                let totalThree = cells[i + (height * 2)].textContent
+                let totalFour = cells[i + (height * 3)].textContent
+                //makes the column into an array, parseInt coverts strings to a number
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                // console.log(totalOne)
+                // console.log(totalTwo)
+                // console.log(totalThree)
+                // console.log(totalFour)
+                //weeds out numbers 
+                let filteredArray = row.filter(cell => cell)
+                let missing = 4 - filteredArray.length
+                let zeros = Array(missing).fill(0)
+                //changed placement zeros and filtered row to move left
+                let newRow = filteredArray.concat(zeros)
+                    
+                //assigns new value of array in indexes  
+                cells[i].textContent = newRow[0]
+                cells[i + height].textContent = newRow[1]
+                cells[i + (height * 2)].textContent = newRow[2]
+                cells[i + (height * 3)].textContent = newRow[3]
                 
                 
             }
@@ -206,10 +203,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     cells[i + width].textContent = 0
                 }
             }
-
+            winCondition()
         }
 
-        //index16 does not exist so loop thorugh <15 
         function combineRowLeftRight() {
             for (let i = 0; i < 15; i++) {
                 //checks if cell is equal to cell next to it 
@@ -221,28 +217,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     cells[i +1 ].textContent = 0  
                 }
             }
+            winCondition()
+        }
+    
+        function winCondition() {
+            for(let i = 0; i < cells.length; i++){
+                if (cells[i].textContent == 4){
+                alert('Game Won')
+                }
+            }
         }
 
-     
-        
-
-
-
-        // move elements in specified direction, grabs all elements in .game-container 
-        //if element has a number !== 0 check leave to the side unless 
-        // if values of child element = to value of other child element create a merge function 
-        
-        //destination.appendChildTo()?
-        //eventTarget.EventListener
-        //Event.stopPropogation 
-        //bubbling, event propogation
-        //giving accessKey a value and then merging it with another?
-        //offset, get bouncingclientRect?
-
-        // console.log(document.querySelectorAll('grid-cell')[1])
-    }
-    
-    
 })
 
 
@@ -250,3 +235,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+// move elements in specified direction, grabs all elements in .game-container 
+//if element has a number !== 0 check leave to the side unless 
+// if values of child element = to value of other child element create a merge function 
+
+//destination.appendChildTo()?
+//eventTarget.EventListener
+//Event.stopPropogation 
+//bubbling, event propogation
+//giving accessKey a value and then merging it with another
+//parseInt converts string to number 
